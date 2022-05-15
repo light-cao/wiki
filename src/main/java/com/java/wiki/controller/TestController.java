8210df045613aaa7d917b61fee1a7deadda1f2f4
light-cao <747807518@qq.com>
2022-05-15 22:28:54 +0800

    2-8 增加了自定义配置项的示例

--- src/main/java/com/java/wiki/controller/TestController.java
+++ src/main/java/com/java/wiki/controller/TestController.java
@@ -1,16 +1,20 @@
 package com.java.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+    @Value("${test.hello:TEST}")
+    private  String testHello;
+
 //    http://127.0.0.1:8880/hello
 //    @RequestMapping("/hello")
     @GetMapping("/hello")
     public String hello() {
-        return "Hello World!";
+        return "Hello World!" + testHello;
     }
 
     @PostMapping("/hello/post")
