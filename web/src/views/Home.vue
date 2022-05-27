e21ce08ce4088ed5790d6229fd55502c2bfa793b
light-cao <747807518@qq.com>
2022-05-27 18:53:07 +0800

    5-4 列表查询接口时候支持动态SQL name参数有值时按name查，没值时查全部 封面小图样式调整

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -47,7 +47,7 @@
     <a-layout-content
             :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
     >
-        <a-list item-layout="vertical" size="large" :grid="{ gutter: 16, column: 4 }" :data-source="ebooks">
+        <a-list item-layout="vertical" size="large" :grid="{ gutter: 20, column: 3 }" :data-source="ebooks">
             <template #renderItem="{ item }">
                 <a-list-item key="item.name">
                     <template #actions>
@@ -113,7 +113,7 @@ export default defineComponent({
       ];
       onMounted(()=> {
         console.log("onMounted");
-        axios.get("http://localhost:8880/ebook/list?name=Spring").then(
+        axios.get("http://localhost:8880/ebook/list").then(
           (response) => {
             const data = response.data;
             ebooks.value = data.content;
@@ -133,3 +133,13 @@ export default defineComponent({
     }
 });
 </script>
+
+<style scoped>
+    .ant-avatar {
+        width: 50px;
+        height: 50px;
+        line-height: 50px;
+        border-radius: 8%;
+        margin: 5px 0;
+    }
+</style>
