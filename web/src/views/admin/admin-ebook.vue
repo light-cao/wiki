389225f56e6b53fb0529aa663530814ec650ae33
light-cao <747807518@qq.com>
2022-05-29 18:39:20 +0800

    6-9 新增雪花算法工具类，完成电子书新增功能

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -3,7 +3,11 @@
     <a-layout style="padding: 0 24px 24px">
       <a-layout-content
               :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
-      >
+      > <p>
+          <a-button type="primary" @click="add()" size="large">
+              新增
+          </a-button>
+        </p>
         <a-table
                 :columns="columns"
                 :row-key="record => record.id"
@@ -171,6 +175,16 @@
         ebook.value = record;
       };
 
+
+      /**
+       * 新增
+       */
+      const add = () => {
+        modalVisible.value = true;
+        ebook.value = {};
+      };
+
+
       onMounted(() => {
         handleQuery({
           page: 1,
@@ -186,6 +200,9 @@
         handleTableChange,
 
         edit,
+        add,
+
+
         ebook,
         modalVisible,
         modalLoading,
