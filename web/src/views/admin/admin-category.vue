63caa34ff4f43c6d3a31d01cbdc5aa4cf9042bfd
light-cao <747807518@qq.com>
2022-05-30 20:26:24 +0800

    7-4 分类管理不显示分页

--- web/src/views/admin/admin-category.vue
+++ web/src/views/admin/admin-category.vue
@@ -6,11 +6,7 @@
       > <p>
           <a-form layout="inline" :model="param">
               <a-form-item>
-                  <a-input v-model:value="param.name" placeholder="名称">
-                  </a-input>
-              </a-form-item>
-              <a-form-item>
-                  <a-button type="primary" @click="handleQuery({page: 1, size: pagination.pageSize})">
+                  <a-button type="primary" @click="handleQuery()">
                       查询
                   </a-button>
               </a-form-item>
@@ -25,9 +21,8 @@
                 :columns="columns"
                 :row-key="record => record.id"
                 :data-source="categorys"
-                :pagination="pagination"
                 :loading="loading"
-                @change="handleTableChange"
+                :pagination="false"
         >
           <template #cover="{ text: cover }">
             <img v-if="cover" :src="cover" alt="avatar" />
@@ -88,11 +83,6 @@
       const param = ref();
       param.value = {};
       const categorys = ref();
-      const pagination = ref({
-        current: 1,
-        pageSize: 10,
-        total: 0
-      });
       const loading = ref(false);
 
       const columns = [
@@ -119,23 +109,13 @@
       /**
        * 数据查询
        **/
-      const handleQuery = (params: any) => {
+      const handleQuery = () => {
         loading.value = true;
-        axios.get("/category/list", {
-          params: {
-            page: params.page,
-            size: params.size,
-            name: param.value.name
-          }
-        }).then((response) => {
+        axios.get("/category/all").then((response) => {
           loading.value = false;
           const data = response.data;
           if (data.success) {
-              categorys.value = data.content.list;
-
-              // 重置分页按钮
-              pagination.value.current = params.page;
-              pagination.value.total = data.content.total;
+              categorys.value = data.content;
           } else {
               message.error(data.message);
           }
@@ -143,16 +123,6 @@
         });
       };
 
-      /**
-       * 表格点击页码时触发
-       */
-      const handleTableChange = (pagination: any) => {
-        console.log("看看自带的分页参数都有啥：" + pagination);
-        handleQuery({
-          page: pagination.current,
-          size: pagination.pageSize
-        });
-      };
         // -------- 表单 ---------
       const category = ref({});
       const modalVisible = ref(false);
@@ -166,10 +136,7 @@
                   modalVisible.value = false;
 
                   //重新加载列表
-                  handleQuery({
-                      page: pagination.value.current,
-                      size: pagination.value.pageSize,
-                  });
+                  handleQuery();
               } else {
                   message.error(data.message);
               }
@@ -199,29 +166,21 @@
               if (data.success) {
 
                   //重新加载列表
-                  handleQuery({
-                      page: pagination.value.current,
-                      size: pagination.value.pageSize,
-                  });
+                  handleQuery();
               }
           });
       };
 
 
       onMounted(() => {
-        handleQuery({
-          page: 1,
-          size: pagination.value.pageSize
-        });
+        handleQuery();
       });
 
       return {
         param,
         categorys,
-        pagination,
         columns,
         loading,
-        handleTableChange,
         handleQuery,
         edit,
         add,
