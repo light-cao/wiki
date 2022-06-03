a497631b8e2e3f7577a24841e624a11c37403a29
light-cao <747807518@qq.com>
2022-06-03 22:38:39 +0800

    8-9 文档管理布局美化 表格紧凑 按钮变小 表单变大 页面打开时就初始化富文本框，z-index变小

--- web/src/views/admin/admin-doc.vue
+++ web/src/views/admin/admin-doc.vue
@@ -4,7 +4,7 @@
       <a-layout-content
               :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
       >
-          <a-row>
+          <a-row :gutter="24">
               <a-col :span="8">
                   <p>
                       <a-form layout="inline" :model="param">
@@ -26,13 +26,14 @@
                           :data-source="level1"
                           :loading="loading"
                           :pagination="false"
+                          size="small"
                   >
-                      <template #cover="{ text: cover }">
-                          <img v-if="cover" :src="cover" alt="avatar" />
+                      <template #name="{ text, record }">
+                          {{record.sort}} {{text}}
                       </template>
                       <template v-slot:action="{ text, record }">
                           <a-space size="small">
-                              <a-button type="primary" @click="edit(record)">
+                              <a-button type="primary" @click="edit(record)" size="small">
                                   编辑
                               </a-button>
                               <a-popconfirm
@@ -41,7 +42,7 @@
                                       cancel-text="否"
                                       @confirm="handleDelete(record.id)"
                               >
-                                  <a-button type="primary" danger>
+                                  <a-button type="primary" danger size="small">
                                       删除
                                   </a-button>
                               </a-popconfirm>
@@ -50,11 +51,20 @@
                   </a-table>
               </a-col>
               <a-col :span="16">
-                  <a-form :model="doc" :label-col="{ span: 6 }" :wrapper-col="{ span: 18 }">
-                      <a-form-item label="名称">
+                  <p>
+                      <a-form layout="inline" :model="param">
+                          <a-form-item>
+                              <a-button type="primary" @click="handleSave">
+                                  保存
+                              </a-button>
+                          </a-form-item>
+                      </a-form>
+                  </p>
+                  <a-form :model="doc" layout="vertical">
+                      <a-form-item>
                           <a-input v-model:value="doc.name" />
                       </a-form-item>
-                      <a-form-item label="父文档">
+                      <a-form-item>
                           <a-tree-select
                                   v-model:value="doc.parent"
                                   style="width: 100%"
@@ -66,10 +76,10 @@
                           >
                           </a-tree-select>
                       </a-form-item>
-                      <a-form-item label="顺序">
-                          <a-input v-model:value="doc.sort" />
+                      <a-form-item>
+                          <a-input v-model:value="doc.sort" placeholder="顺序"/>
                       </a-form-item>
-                      <a-form-item label="内容">
+                      <a-form-item>
                           <div id="content"></div>
                       </a-form-item>
                   </a-form>
@@ -119,16 +129,8 @@
       const columns = [
         {
           title: '名称',
-          dataIndex: 'name'
-        },
-        {
-          title: '父文档',
-          key: 'parent',
-          dataIndex: 'parent'
-        },
-        {
-          title: '顺序',
-          dataIndex: 'sort'
+          dataIndex: 'name',
+          slots: { customRender: 'name'}
         },
         {
           title: 'Action',
@@ -185,10 +187,10 @@
       const modalVisible = ref(false);
       const modalLoading = ref(false);
       const editor = new E('#content');
+      editor.config.zIndex = 0;
 
 
-
-      const handleModalOk = () => {
+      const handleSave = () => {
         modalLoading.value = true;
           axios.post("/doc/save", doc.value).then((response) => {
               modalLoading.value = false;
@@ -282,9 +284,6 @@
 
         // 为选择树添加一个"无"
         treeSelectData.value.unshift({id: 0, name: '无'});
-        setTimeout(function () {
-            editor.create();
-        }, 100);
       };
 
 
@@ -301,9 +300,6 @@
 
         // 为选择树添加一个"无"
         treeSelectData.value.unshift({id: 0, name: '无'});
-        setTimeout(function () {
-            editor.create();
-        }, 100);
       };
 
       const handleDelete = (id : number) => {
@@ -331,6 +327,7 @@
 
       onMounted(() => {
         handleQuery();
+        editor.create();
       });
 
       return {
@@ -347,7 +344,7 @@
         doc,
         modalVisible,
         modalLoading,
-        handleModalOk,
+        handleSave,
 
         handleDelete,
 
