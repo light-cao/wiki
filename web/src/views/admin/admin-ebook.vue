c79085746085b3a6e1ad182411d248fad1769bee
light-cao <747807518@qq.com>
2022-06-03 16:38:59 +0800

    8-6 删除某个文档时，将其所有的文档全部删除

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -51,7 +51,7 @@
                    cancel-text="否"
                    @confirm="handleDelete(record.id)"
               >
-              <a-button type="danger">
+              <a-button type="primary" danger>
                   删除
               </a-button>
               </a-popconfirm>
