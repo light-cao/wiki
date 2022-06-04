93c027f50f4e2a5c9f193f7b461c2114674b611f
light-cao <747807518@qq.com>
2022-06-04 11:11:21 +0800

    8-12 文档页面和文档管理页面，只能查当前电子书下的所有文档

--- src/main/java/com/java/wiki/service/DocService.java
+++ src/main/java/com/java/wiki/service/DocService.java
@@ -34,8 +34,9 @@ public class DocService {
     @Resource
     private SnowFlake snowFlake;
 
-    public List<DocQueryResp> all() {
+    public List<DocQueryResp> all(Long ebookId) {
         DocExample docExample = new DocExample();
+        docExample.createCriteria().andEbookIdEqualTo(ebookId);
         docExample.setOrderByClause("sort asc");
         DocExample.Criteria criteria = docExample.createCriteria();
         List<Doc> docList = docMapper.selectByExample(docExample);
